a94c97c965928e1ef9a5b31bafa56c587b5cb9a7
lverghese <loganverghese@gmail.com>
2021-08-01 19:13:55 -0400

    movie data collection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 const bookAPIKey = "AIzaSyDcXOeyScItwcFMcUTf8T0ESWNi1x22kvU"
 const movieAPIKey = "9460c855"
-var userTitleSearch = ""
-var type = ""
 
-var movieInputEl = document.querySelector("#movieName")
-
+var movieInputEl = document.getElementById("movieSearch");
+var releaseYear = document.getElementById("releaseYear");
+var movieRating = document.getElementById("movieRating");
+var boxOffice = document.getElementById("boxOffice");
+var smallPlot = document.getElementById("smallPlot");
+var director = document.getElementById("director");
 
 //function to get info from the book api
 
@@ -25,24 +27,51 @@ var getBook = function() {
         }
       })
       .catch(function(error) {
-          alert('Unable to connect to movie');
+          alert('Unable to connect to book api');
       });
     }
 
 
+
+//retrieving the movie title and sending it to the html
+
+var getMovieTitle = function() {
+
+    //getting a value from the input element
+    var title = movieInputEl.value.trim();
+
+    if (title) {
+        getMovieData(title);
+        movieInputEl.value = ''
+    } else {
+        alert("Please input a movie title")
+    }
+}
+
+
+
+
+
+
 //function to get info from the movie api
 
-var getMovie = function() {
+var getMovieData = function(movie) {
     
-    var movieUrl = "http://www.omdbapi.com/?apikey=" + movieAPIKey + "&t=batman+begins";
+    var movieUrl = "http://www.omdbapi.com/?apikey=" + movieAPIKey + "&t=batman+begins" + "&r=json";
 
     fetch(movieUrl)
     .then(function(response) {
         //if response was successful
         if (response.ok) {
             console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
+            response.json().then(function(title) {
+                console.log(title);
+                releaseYear.innerHTML = title.Released;
+                boxOffice.innerHTML = title.BoxOffice;
+                smallPlot.innerHTML = title.Plot;
+                movieRating.innerHTML = title.Rated;
+                director.innerHTML = title.Director;
+                
             });
         } else {
             alert('Error: ' + response.statusText);
@@ -53,5 +82,5 @@ var getMovie = function() {
       });
     }
 
-    getMovie();
+    getMovieData();
     getBook();
