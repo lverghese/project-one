6f1528f39cf8574f386d5777796f3b5da0ae90e1
SadiaSookram <sadiamsookram@gmail.com>
2021-08-01 14:48:52 -0400

    Add Const Var for API Keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
+var bookAPIKey = "j6Kh3WHlTmst7fqijtBiGQZe8BFyQjzc"
+const movieApiKey = "53e27a88"
+var userTitleSearch = ""
+var type = ""
+
+
 //function to get info from the book api
-
-
-
+fetch("https://api.nytimes.com/svc/books/v3/reviews.json?title=${userTitleSearch}&api-key=j6Kh3WHlTmst7fqijtBiGQZe8BFyQjzc")
+      .then(function(response){
+          console.log('Success!');
+      })
+      .catch(function(error){
+          console.log('API trouble'+ error)
+      });
+    
 //function to get info from the movie api
-
-
+fetch("http://www.omdbapi.com/?tt0108037&apikey=53e27a88")
+      .then(function(response){
+          console.log('Success!');
+      })
+      .catch(function(error){
+          console.log('API trouble'+ error)
+      });
